7795cf5435ec1b0ff4050c0fa0d4e34bd2176fe0
AyuAndrinova <145889082+AyuAndrinova@users.noreply.github.com>
2024-05-24 19:57:28 +0700

    Update main.php

---
+++ function/main.php
@@ -0,0 +1,73 @@
+<?php
+function view($page, $data=[]) {
+    extract($data);
+    include 'view/'.$page.'.php';
+}
+
+class Router { 
+    public static $urls = [];
+
+    function __construct() {
+        $url = implode("/", 
+            array_filter(
+                explode("/", 
+                    str_replace($_ENV['BASEDIR'], "", 
+                        parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+                    )
+                ), 'strlen'
+            )
+        );
+
+        if (!in_array($url, self::$urls['routes'])) {
+            header('Location: '.BASEURL);
+        }
+
+        $call = self::$urls[$_SERVER['REQUEST_METHOD']][$url];
+        $call();
+    }
+    public static function url($url, $method, $callback) {
+        if ($url == '/') { $url = ''; }
+        self::$urls[strtoupper($method)][$url] = $callback;
+        self::$urls['routes'][] = $url;
+        self::$urls['routes'] = array_unique(self::$urls['routes']);
+    }
+}
+
+function urlpath($path) {
+    require_once 'config/static.php';
+    return BASEURL.$path;
+}
+
+function freshdb() {
+    require_once 'model/user_model.php';
+    require_once 'model/contact.php';
+    global $conn;
+    $conn->query('DELETE FROM users');
+    $conn->query('ALTER TABLE users AUTO_INCREMENT = 1');
+    $conn->query('DELETE FROM contacts');
+    $conn->query('ALTER TABLE contacts AUTO_INCREMENT = 1');
+
+    User::register([
+        'username' => $_ENV['USERNAME'],
+        'email' => $_ENV['EMAIL'],
+        'password' => $_ENV['PASSWORD']
+    ]);
+
+    $contacts = array(
+        [1, '081234567890', 'Sarinah Rahayu'],
+        [2, '082733594216', 'Rojak Bima'],
+        [3, '082156894327', 'Amina Lukman'],
+        [4, '082233478967', 'Dika Mahardika'],
+        [5, '082790823676', 'Liliana Liem']
+    );
+
+    foreach ($contacts as $c) {
+        Contact::insert([
+                'id' => 1,
+                'no' => $c[0],
+                'owner' => $c[1],
+                ]);
+    }
+    view('freshdb');
+    session_destroy();
+}
